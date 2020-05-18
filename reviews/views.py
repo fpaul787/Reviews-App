379a4b29477b6bb5a2b574a14a500d1c7a23136c
Frantz <fpaul787@gmail.com>
2020-05-18 16:24:43 -0400

    added post in view and changed home html to loop through post

--- reviews/views.py
+++ reviews/views.py
@@ -2,13 +2,27 @@ from django.shortcuts import render
 
 
 # Create your views here.
-
+posts = [
+    {
+        'author': 'Frantz',
+        'title': 'The Miseducation of Lauryn Hill Review',
+        'content': "First post content",
+        'date_posted': "May 10, 2020"
+    },
+    {
+        'author': 'Frantz',
+        'title': 'Reasonable Doubt Review',
+        'content': "First post content",
+        'date_posted': "May 10, 2020"
+    }
+]
 def home(request):
     data = {
         'name': "Frantz"
     }
 
     context = {
-        'data': data
+        'data': data,
+        'posts': posts
     }
     return render(request, 'reviews/home.html' ,context)
