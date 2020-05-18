8a1609087687f8f098f4c7ae24c669d8c93f4be6
Frantz <fpaul787@gmail.com>
2020-05-18 15:27:01 -0400

    added base.html

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def home(request):
+    data = {
+        'name': "Frantz"
+    }
+
+    context = {
+        'data': data
+    }
+    return render(request, 'reviews/base.html' ,context)
