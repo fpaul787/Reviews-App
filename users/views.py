6c94434c741ba123c59ac484c0e4baf361a50659
Frantz <fpaul787@gmail.com>
2020-05-19 15:25:50 -0400

    added csrf token, changed redirect url, included alert message

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account successfully created for {username}')
-            return redirect('blog-home')
+            return redirect('reviews-home')
     else:
         form = UserCreationForm()
 
