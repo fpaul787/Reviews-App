5b87a94a0f2f352b25556dc34a1681457fac5025
Frantz <fpaul787@gmail.com>
2020-05-20 15:19:48 -0400

    added new update forms

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, UserProfileUpdateFrom
 from django.contrib import messages
 
 def register(request):
@@ -25,4 +25,22 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        user_update_form = UserUpdateForm(request.POST, instance=request.user)
+        user_profile_update_form = UserProfileUpdateFrom(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_update_form.is_valid() and user_profile_update_form.is_valid():
+            user_update_form.save()
+            user_profile_update_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+    else:
+        user_update_form = UserUpdateForm(instance=request.user)
+        user_profile_update_form = UserProfileUpdateFrom(instance=request.user.profile)
+
+    context = {
+        'u_form': user_update_form,
+        'p_form': user_profile_update_form
+    }
+
+    return render(request, 'users/profile.html', context)
