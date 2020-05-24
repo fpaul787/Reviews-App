82c30805f1599b21f5a503e819abba244d168116
Frantz <fpaul787@gmail.com>
2020-05-24 18:43:27 -0400

    removed sqlite file and change models to show last_updated in admin

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from reviews.models import Review
 
 # Create your models here.
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.ForeignKey(Review)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.user.username)
--- reviews/admin.py
+++ reviews/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Review
 
-admin.site.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    
+    list_display = ["__str__", "last_updated"]
+    readonly_fields = ["last_updated"]
+    class Meta:
+        model = Review
+
+admin.site.register(Review, ReviewAdmin)
