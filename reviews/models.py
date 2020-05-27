1b210546defb9e4e7605c44f34af5460d528ce72
Frantz <fpaul787@gmail.com>
2020-05-26 23:57:38 -0400

    added slugs to model and recreated db

--- reviews/models.py
+++ reviews/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.db.models.signals import pre_save # before model is saved, do something
 
 # test
+from django.utils.text import slugify
 class Review(models.Model):
     title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
     content = models.TextField()
     last_updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     date_posted = models.DateTimeField(default=timezone.now)
@@ -12,3 +15,21 @@ class Review(models.Model):
 
     def __str__(self):
         return self.title
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Review.objects.filter(slug=slug).order_by("-id")
+    exists = qs.exists()
+    if exists:
+        new_slug = "%s-%s" %(slug, qs.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+
+# anytime save is called, this is called before
+def pre_save_review_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+pre_save.connect(pre_save_review_receiver, sender=Review)
