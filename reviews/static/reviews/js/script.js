51c5b7e1d2eea7ebdbe50bd22b71b2d13dfbe04a
Frantz <fpaul787@gmail.com>
2020-06-02 21:39:42 -0400

    added script to base to set localstate

--- reviews/static/reviews/js/script.js
+++ reviews/static/reviews/js/script.js
@@ -3,8 +3,23 @@ function trial()
     document.getElementById("demo").innerHTML = "Hi, I am your javascript from an external source";
 } 
 
+function checkboxClick(e)
+{
+    e.preventDefault();
+
+    if (e.target.checked){
+        localStorage.setItem('checked', true)
+        // localStorage.checked = true;
+    }else{
+        localStorage.setItem('checked', false)
+        // localStorage.checked = false;
+    }
+}
+
 window.onload = function() {
-    // this.alert('Page is loaded')
+    //this.alert('Page is loaded')
+
+    
 
     if (this.document.getElementById('messageElement').classList.length > 0){
             
@@ -12,7 +27,34 @@ window.onload = function() {
         this.document.getElementById('messageElement').remove();
         },
         3000);
-        
-
-    }
+    }  
 }
+
+function App() {
+
+}
+
+setState = function(state) {
+    localStorage.setItem('checked', state);
+}
+  
+getState = function() {
+   return localStorage.getItem('checked');
+}
+function init(){
+    // var app = new App();
+    var state = getState();
+    var checkbox = document.getElementById('cb1');
+    
+
+
+    if(state == 'true'){
+        checkbox.checked = true;
+    }
+
+    checkbox.addEventListener('click', function() {
+        setState(checkbox.checked)
+    })
+}
+
+init();
